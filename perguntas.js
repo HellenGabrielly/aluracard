4a11bde69e97a3fa47e89d569162f5b002276e92
HellenGabrielly <0000109315195xsp@al.educacao.sp.gov.br>
2024-11-05 09:13:26 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    '?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    'A capital do Japão fica em Tóquio'
+)
+
+criaCartao(
+    'Comida',
+    'De qual tipo de alimento o tomate se encaixa?',
+    'O tomate é considerado um fruto'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Ingles?',
+    'Eu te amo em Ingles fica I love you'
+)
